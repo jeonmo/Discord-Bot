d773d1fcd1e3ce23cd7560015266a4dece472d6f
hjy1518 <130831979+hjy1518@users.noreply.github.com>
2023-05-28 22:31:53 +0900

    Notice.py

--- Notice.py
+++ Notice.py
@@ -12,20 +12,24 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    # 봇이 준비되었을 때 호출되는 이벤트 핸들러
     print(f'봇이 로그인했습니다. (ID: {bot.user.id})')
 
 @bot.event
 async def on_message(message):
+    # 메시지가 도착했을 때 호출되는 이벤트 핸들러
     if message.author.bot:
         return
 
     if message.content == "동의대 공지사항":
+        # "동의대 공지사항" 메시지가 도착하면 공지사항 정보를 가져와서 전송
         result = await get_notice_information()
         await message.channel.send(result)
 
     await bot.process_commands(message)
 
 async def get_notice_information():
+    # 공지사항 정보를 가져오는 비동기 함수
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     notice_rows = soup.find_all("tr")
@@ -35,6 +39,7 @@ async def get_notice_information():
         columns = row.find_all("td")
 
         if len(columns) > 0:
+            # 각 행에서 필요한 정보 추출
             serial_number_element = row.find("th", {"scope": "row"})
             serial_number = serial_number_element.text.strip() if serial_number_element else "N/A"
             title_element = columns[0].find("a")
@@ -42,9 +47,10 @@ async def get_notice_information():
             author = columns[1].text.strip() if len(columns) > 1 else "N/A"
             date = columns[2].text.strip() if len(columns) > 2 else "N/A"
             views = columns[3].text.strip() if len(columns) > 3 else "N/A"
+
+            # 정보를 포맷팅하여 결과 문자열에 추가
             result += f"순번: {serial_number}\n제목: {title}\n작성자: {author}\n작성일: {date}\n조회수: {views}\n-------------------\n"
 
     return result
 
 bot.run('MTEwMzc4MDIxMzkwNjczNTE0NA.GwF9Im.3JNSiieH36Se7Vfi-pQ7c36ErAWDS0rErgmDco')
-
