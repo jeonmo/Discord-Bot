111803dfe40e0a08e7f5e37cd15c2a4aff59a6a1
dfredd <132764734+dfredd@users.noreply.github.com>
2023-05-29 17:02:08 +0900

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -12,9 +12,9 @@ async def facebook_search(message, client):
     url = "https://www.deu.ac.kr/www"
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--disable-notifications")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("--disable-notifications")             #알림 표시를 비활성화하는 옵션
+    options.add_argument("--ignore-certificate-errors")         #인증서 오류를 무시하는 옵션을 추가합니다.
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])#ChromeDriver 로그를 출력하지 않도록 설정하는 옵션
     service = webdriver.chrome.service.Service(DRIVER_PATH)
     service.start()
     driver = webdriver.Chrome(service=service, options=options)
@@ -22,8 +22,8 @@ async def facebook_search(message, client):
     try:
         driver.get(url)
 
-        element = driver.find_element(By.XPATH, '//*[@id="nav-ctrl01-tab"]')
-        driver.execute_script("arguments[0].click();", element)
+        element = driver.find_element(By.XPATH, '//*[@id="nav-ctrl01-tab"]')#XPath를 사용하여 요소를 찾습니다.
+        driver.execute_script("arguments[0].click();", element)    # JavaScript를 실행하여 요소를 클릭
 
         time.sleep(5)
 
@@ -31,25 +31,25 @@ async def facebook_search(message, client):
         html.send_keys(Keys.END)
         time.sleep(2)
 
-        feed_elements = driver.find_elements(By.XPATH, '//*[@id="nav-ctrl01"]/div/div')
+        feed_elements = driver.find_elements(By.XPATH, '//*[@id="nav-ctrl01"]/div/div') #XPath를 사용하여 피드 요소들을 탐색
 
         feed_texts = []
         feed_images = []
 
         for element in feed_elements:
-            text_element = element.find_element(By.XPATH, ".//p[contains(@class, 'tit')]")
+            text_element = element.find_element(By.XPATH, ".//p[contains(@class, 'tit')]")#요소에서 텍스트를 포함하는 요소를 찾습니다.
             text = text_element.text
             feed_texts.append(text)
 
-            image_elements = element.find_elements(By.CSS_SELECTOR, 'div.thum img')
+            image_elements = element.find_elements(By.CSS_SELECTOR, 'div.thum img')#요소에서 이미지를 포함하는 요소를 찾습니다.
             images = [image.get_attribute('src') for image in image_elements]
             feed_images.append(images)
 
         results = []
-        if feed_texts:
+        if feed_texts:          #피드 텍스트가 있을때만 실시
             for i, text in enumerate(feed_texts):
                 result = f'Text {i+1}: {text}\n'
-                if feed_images[i]:
+                if feed_images[i]:  #해당 인덱스의 피드에 이미지가 있는 경우에만 실시
                     for image in feed_images[i]:
                         result += f'Image: {image}\n'
                 results.append(result)
